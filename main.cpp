fd085a416c52840b8a08453f9aedcacb676a2ccb
alex <alex@example.com>
2018-12-20 15:40:31 +0300

    Add not ready option -a
    Add option -a to analyze file that we can recive by redirect include-explorer output to file.
    The result of this option is list of include files from some target directory.
    For example (+: valid, -:invalid)
    
    + .    target_project/main.h - project file from project file
    + ..   target_project/foo.h - project file from project file
    + ...  /usr/include/stdio.h - system file from project file
    - .... /usr/include/wchar.h - system file from system file
    + ..   /usr/include/stdio.h - system file from project file
    
    Using:
    ./include-explorer -f test/compile_commands.json > res.txt 2>&1
    ./include-explorer -a res.txt include-explorer/test
    
    Now it does not work properly. Now it only show include files with path and level.

--- main.cpp
+++ main.cpp
@@ -25,6 +25,30 @@ int main(int argc, char* argv[]) {
   std::string json_file_path = "./compile_commands.json";
   std::string compiler_command = "";
   std::string json_compiler_name = "";
+
+  std::ifstream file_to_analyze;
+  std::ofstream result_file("include-explorer-result.txt");
+  std::string file_to_analyze_path = "./tmpMakefileResult.txt";
+  int analyze_flag = 0;
+  int processing_includes = 0;
+
+  size_t cd_start_pos = 0;          
+  size_t file_path_start_pos = 0;
+  size_t file_path_end_pos = 0;
+  std::string file_path = "";
+
+  size_t include_file_start_pos = 0;
+  size_t include_file_end_pos = 0;
+  std::string include_file_path = "";
+
+  int include_file_is_system = 0;
+  int include_file_level_num = 0;
+  std::string include_file_level_str;
+  int prev_include_file_level_num = 0;
+  std::string prev_include_file_level_str;
+  int prev_include_file_is_system = 0;
+  std::string target_directory; //path to directory (part of it or full path) that contains analyzed project
+
   int i = 0;
 
   //parse arguments for options
@@ -44,12 +68,20 @@ int main(int argc, char* argv[]) {
       {
         compiler_command = std::string(argv[i+1]);
       }
+      else if (argument_str.compare("-a") == 0)
+      {
+        analyze_flag = 1;
+        file_to_analyze_path = std::string(argv[i+1]);
+        target_directory = std::string(argv[i+2]);
+      }
       else if (argument_str.compare("-h") == 0)
       {
         std::cout << std::endl << "Using: ./include-explorer [-f path/to/json/file/dir/json_file_name.json] [-p compiler_prefix]" << std::endl;
         std::cout << "-f - full path to json file with compile commands. Default: \"./compile_commands.json\"" << std::endl;
         std::cout << "-p - full path to compiler prefix. Default: \"\"" << std::endl;
-        std::cout << "Example : ./include-explorer -f test/compile_commands.json -p /opt/arm-linux-gnueabihf/arm-linux-gnueabihf-" << std::endl << std::endl;
+        std::cout << "-a - analyze result file" << std::endl;
+        std::cout << "Example : ./include-explorer -f test/compile_commands.json -p /opt/arm-linux-gnueabihf/arm-linux-gnueabihf-" << std::endl;
+        std::cout << "Example : ./include-explorer -a ./tmpMakefileResult.txt include-explorer/test" << std::endl << std::endl;
         return -1;
       }
     }
@@ -62,133 +94,221 @@ int main(int argc, char* argv[]) {
       std::cout << std::endl << "**********************************************************************" << std::endl << std::endl;
   }
 
-  in_comp_db_json.open(json_file_path);
-
-  if (in_comp_db_json.is_open())
+  if (!analyze_flag)
   {
-    out_comp_db_json << "{" << std::endl;
-    out_comp_db_json << "\"commands\":";
+    in_comp_db_json.open(json_file_path);
 
-    while ( getline (in_comp_db_json,line) )
+    if (in_comp_db_json.is_open())
     {
-       out_comp_db_json << line << std::endl;
+      out_comp_db_json << "{" << std::endl;
+      out_comp_db_json << "\"commands\":";
+
+      while ( getline (in_comp_db_json,line) )
+      {
+         out_comp_db_json << line << std::endl;
+      }
+      
+      out_comp_db_json << std::endl << "}";
+      
+      in_comp_db_json.close();
+      out_comp_db_json.close();
     }
+    else std::cout << "Unable to open file"; 
+
+
+    //Parse output file by rapidjson.
+    //Modify commands by adding cross-compiler name and option -H
+    //Put new commands to Makefile to perform this command.
+    std::ifstream ifs("tmpJson.json");
+    IStreamWrapper isw(ifs);
+    Document documentFromFile;
+
+    documentFromFile.ParseStream(isw);
+
+    // Using a reference for consecutive access is handy and faster.
+    const Value& a = documentFromFile["commands"];
+    assert(a.IsArray());
+
+    std::ofstream makefile("tmpMakefile");
+
+
+    makefile << "CC=" << compiler_command << "gcc" << std::endl << std::endl;
+    makefile << "CXX=" << compiler_command << "g++" << std::endl << std::endl;
+    makefile << "all:" << std::endl;
+
+
+    std::string command = "";
+    std::string directory = "";
+    //int commandsCount = a.Size(); ////to output file number 
+    std::cout << "-include files list:" << std::endl;
+    for (auto& record : a.GetArray()) {
+
+      //makefile << "\t" << "$echo " << i++ << "/" << commandsCount << std::endl;     //to output file number
+
+    	makefile << "\t";
+
+    	directory = record["directory"].GetString();
+    	makefile << "cd " << directory << " && ";
+
+      //read command from json file
+      command = record["command"].GetString();
+      
+      //write gcc/g++ command instead cc/c++ 
+      json_compiler_name = command.substr(0,3);
+      if (json_compiler_name.compare("cc ") == 0)
+      {
+        makefile << "$(CC)";
+      }
+      else if (json_compiler_name.compare("c++") == 0)
+      {
+        makefile << "$(CXX)";
+      }
+      else
+      {
+        std::cout << "Error. Unknown compiler command in json file: " << json_compiler_name << std::endl;
+        return -1;
+      }
+
+      //process -include option
+      size_t search_start_pos = 0;
+      size_t inc_opt_start_pos = 0;
+      size_t inc_filename_start_pos = 0;
+      size_t inc_filename_end_pos = 0;
+      std::string search_pattern = std::string(" -include ");
+      std::string inc_filename = "";
+      while ((inc_opt_start_pos = command.find(search_pattern, search_start_pos)) != std::string::npos)
+      {
+        inc_filename_start_pos = command.find_first_not_of(" ",inc_opt_start_pos + 10);
+        inc_filename_end_pos = command.find_first_of(" ",inc_filename_start_pos + 1);
+        inc_filename = command.substr(inc_filename_start_pos, inc_filename_end_pos - inc_filename_start_pos);
+        //inc_filename = command.substr(inc_filename_start_pos, 10);
+        search_start_pos = inc_opt_start_pos + 5;
+        std::cout << inc_filename << std::endl;
+      }
+
+      //process \" in macro definition in gcc command (-D=\"Hello World\")
+      search_start_pos = 0;
+      size_t start_pos = 0;
+      search_pattern = std::string("\\\"");
+      while ((start_pos = command.find("\\\"", search_start_pos)) != std::string::npos)
+      {
+        command.replace(start_pos,2,"\"\\\"");
+        search_start_pos = start_pos + 2;
+      }
+
+      //write the rest of the command
+      makefile << " ";
+      makefile << command.substr(3);
+      makefile << " -H ";
+      makefile << "-fsyntax-only ";
+      makefile << std::endl;
+    }
+
+    makefile.close();
     
-    out_comp_db_json << std::endl << "}";
+    //Perform generated Makefile
+
+    //show includes that directly comes from gcc command
+    //because they are not shown after preprocessing or after using -H option
+
+    std::cout << std::endl;
+    std::cout << "Includes that come directly from gcc command:" << std::endl;
+
+    system("make -f tmpMakefile 2>&1 |  grep --color \"\\-include\"");
     
-    in_comp_db_json.close();
-    out_comp_db_json.close();
+    std::cout << std::endl << "Other levels includes:" << std::endl;
+    //show other levels includes that contains some keyword (for example "lima")
+    //system("make -f tmpMakefile 2>&1 | grep '^\\.\\+ ' | grep lima | sort -u");
+    system("make -f tmpMakefile");
+
+    std::cout << std::endl;
+    std::cout << "Finish." << std::endl;
   }
-  else std::cout << "Unable to open file"; 
+  else //analyze_flag = 1
+  {
+    file_to_analyze.open(file_to_analyze_path);
 
-
-  //Parse output file by rapidjson.
-  //Modify commands by adding cross-compiler name and option -H
-  //Put new commands to Makefile to perform this command.
-  std::ifstream ifs("tmpJson.json");
-  IStreamWrapper isw(ifs);
-  Document documentFromFile;
-
-  documentFromFile.ParseStream(isw);
-
-  // Using a reference for consecutive access is handy and faster.
-  const Value& a = documentFromFile["commands"];
-  assert(a.IsArray());
-
-  std::ofstream makefile("tmpMakefile");
-
-
-  makefile << "CC=" << compiler_command << "gcc" << std::endl << std::endl;
-  makefile << "CXX=" << compiler_command << "g++" << std::endl << std::endl;
-  makefile << "all:" << std::endl;
-
-
-  std::string command = "";
-  std::string directory = "";
-  //int commandsCount = a.Size(); ////to output file number 
-  std::cout << "-include files list:" << std::endl;
-  for (auto& record : a.GetArray()) {
-
-    //makefile << "\t" << "$echo " << i++ << "/" << commandsCount << std::endl;     //to output file number
-
-  	makefile << "\t";
-
-  	directory = record["directory"].GetString();
-  	makefile << "cd " << directory << " && ";
-
-    //read command from json file
-    command = record["command"].GetString();
-    
-    //write gcc/g++ command instead cc/c++ 
-    json_compiler_name = command.substr(0,3);
-    if (json_compiler_name.compare("cc ") == 0)
+    if (file_to_analyze.is_open())
     {
-      makefile << "$(CC)";
-    }
-    else if (json_compiler_name.compare("c++") == 0)
-    {
-      makefile << "$(CXX)";
-    }
-    else
-    {
-      std::cout << "Error. Unknown compiler command in json file: " << json_compiler_name << std::endl;
-      return -1;
-    }
 
-    //process -include option
-    size_t search_start_pos = 0;
-    size_t inc_opt_start_pos = 0;
-    size_t inc_filename_start_pos = 0;
-    size_t inc_filename_end_pos = 0;
-    std::string search_pattern = std::string(" -include ");
-    std::string inc_filename = "";
-    while ((inc_opt_start_pos = command.find(search_pattern, search_start_pos)) != std::string::npos)
-    {
-      inc_filename_start_pos = command.find_first_not_of(" ",inc_opt_start_pos + 10);
-      inc_filename_end_pos = command.find_first_of(" ",inc_filename_start_pos + 1);
-      inc_filename = command.substr(inc_filename_start_pos, inc_filename_end_pos - inc_filename_start_pos);
-      //inc_filename = command.substr(inc_filename_start_pos, 10);
-      search_start_pos = inc_opt_start_pos + 5;
-      std::cout << inc_filename << std::endl;
-    }
+      //search for Other levels includes
+      while ( getline (file_to_analyze,line) )
+      {
+          if (line.find ("Other levels") != std::string::npos)
+            break;
+      } 
 
-    //process \" in macro definition in gcc command (-D=\"Hello World\")
-    search_start_pos = 0;
-    size_t start_pos = 0;
-    search_pattern = std::string("\\\"");
-    while ((start_pos = command.find("\\\"", search_start_pos)) != std::string::npos)
-    {
-      command.replace(start_pos,2,"\"\\\"");
-      search_start_pos = start_pos + 2;
-    }
+      while ( getline (file_to_analyze,line) )
+      {
 
-    //write the rest of the command
-    makefile << " ";
-    makefile << command.substr(3);
-    makefile << " -H ";
-    makefile << "-fsyntax-only ";
-    makefile << std::endl;
+        if (line.find ("cd ") == 0)
+        {
+          processing_includes = 1;
+          result_file << std::endl << std::endl;
+        }
+        else if (line.find ("Multiple ") != std::string::npos)
+        {
+          processing_includes = 0;
+        }
+
+        if(!processing_includes)
+          continue;
+        else
+        {
+          //get path from cd command
+          if (line.find ("cd ") == 0) //cd command
+          {
+            cd_start_pos = line.find("cd", 0);
+            file_path_start_pos = line.find_first_of(" ",0) + 1;
+            file_path_end_pos = line.find_first_of(" ",file_path_start_pos);
+            file_path = line.substr(file_path_start_pos, file_path_end_pos - file_path_start_pos);
+            //result_file << file_path << std::endl;
+          }
+          else //processing include file
+          {
+            //get include file path
+            include_file_start_pos = line.find_first_of(" ",0) + 1;
+            include_file_end_pos = line.find_first_of(" ",include_file_start_pos);
+            include_file_path = line.substr(include_file_start_pos, include_file_end_pos - include_file_start_pos);
+            if (include_file_path.find("/") != 0) //add directory path if include file path is relative 
+              include_file_path = file_path + "/" + include_file_path;
+
+            //get include file level
+            include_file_level_str = line.substr(0,line.find_first_of(" ",0));
+            include_file_level_num = include_file_level_str.length();
+            //result_file << include_file_level_str << " " << include_file_level_num <<  std::endl;
+
+            //define if file is in target directory
+            if (include_file_path.find(target_directory,0) != std::string::npos)
+            {
+              include_file_is_system = 0;
+            }
+            else
+            {
+              include_file_is_system = 1;
+            }
+
+            if ((include_file_is_system && !prev_include_file_is_system) || !include_file_is_system)
+              result_file << include_file_path << std::endl;
+
+
+            result_file << line << std::endl;
+          }
+        }
+
+
+         
+      }
+      
+      file_to_analyze.close();
+      result_file.close();
+    }
+    else 
+    {
+        std::cout << "Unable to open file to analyze" << std::endl; 
+        return 0;
+    }
   }
-
-  makefile.close();
-  
-  //Perform generated Makefile
-
-  //show includes that directly comes from gcc command
-  //because they are not shown after preprocessing or after using -H option
-
-  std::cout << std::endl;
-  std::cout << "Includes that come directly from gcc command:" << std::endl;
-
-  system("make -f tmpMakefile 2>&1 |  grep --color \"\\-include\"");
-  
-  std::cout << std::endl << "Other levels includes:" << std::endl;
-  //show other levels includes that contains some keyword (for example "lima")
-  //system("make -f tmpMakefile 2>&1 | grep '^\\.\\+ ' | grep lima | sort -u");
-  system("make -f tmpMakefile");
-
-  std::cout << std::endl;
-  std::cout << "Finish." << std::endl;
   
   return 1;
 
